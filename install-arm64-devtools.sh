a318894f1644f2de2816062d2825ebb231afad49
Jindřich Skácel <ronny63@gmail.com>
2016-03-16 23:45:21 +0100

    Update install-arm64-devtools.sh
    Added missing gcc, but anyway, I think, this can be a oneliner with installation of golang.

--- install-arm64-devtools.sh
+++ install-arm64-devtools.sh
@@ -5,4 +5,4 @@ sudo apt-get install -y golang
 
 # Docker dependencies
 #   ATTENTION: this creates a new /boot/initrd.img, takes a long time
-sudo apt-get install -y btrfs-tools libsqlite3-dev libdevmapper-dev
+sudo apt-get install -y btrfs-tools libsqlite3-dev libdevmapper-dev gcc
